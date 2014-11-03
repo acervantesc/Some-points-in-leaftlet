bda3ffa88109dd7ad5c83d2e9a910faa415f6673
acervantesc <acervantesc@gmail.com>
2014-11-02 22:46:57 -0500

    First commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([40.718829,-73.969419], 12);
+var map = L.map('map').setView([40.718829,-73.969419], 13);
 
 L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
     maxZoom: 18
@@ -40,76 +40,68 @@ var officeIcon = L.icon({
 });
 
 
+//Locations
 
+//office
+var officeMarker = L.marker([40.758189,-73.981983], {icon: officeIcon}).addTo(map);
+//marker.bindPopup("<b>WSJ</b><br>I work here")
 
+//pratt
 var prattMarker = L.marker([40.691158,-73.963100], {icon: prattIcon}).addTo(map);
 	//marker.bindPopup("<b>Pratt!</b><br>I come here every week.")
 
+//home
 var homeMarker = L.marker([40.686887,-73.949153], {icon: homeIcon}).addTo(map);
 //marker.bindPopup("<b>300 Quincy!</b><br>I live here.")
 
 
-var officeMarker = L.marker([40.758189,-73.981983], {icon: officeIcon}).addTo(map);
-//marker.bindPopup("<b>WSJ</b><br>I work here")
+
+
+//listeners for sidebar hovers
+
+officeMarker.on('mouseover',function(){
+	$('.location').toggleClass('hover');
+})
+
+prattMarker.on('mouseover',function(){
+	$('.location').toggleClass('hover');
+})
+
+homeMarker.on('mouseover',function(){
+	$('.location').toggleClass('hover');
+})
 
 
 
-var circle = L.circle([40.746926,-73.945386],600,{
-	color: 'red',
-	stroke: 0,
-	fillCollor: '#f03',
-	fillOpacity: 0.5
-}) .addTo(map);
-
-circle.bindPopup("<b>Clinton Hill!</b><br>School Area")
 
 
-var circle = L.circle([40.710372,-74.007657],600,{
-	color: 'red',
-	stroke: 0,
-	fillCollor: '#f03',
-	fillOpacity: 0.5
-}) .addTo(map);
+//recenter the map on click
 
-circle.bindPopup("<b>WSJ</b><br>I work here")
-
-
-var polygon = L.polygon([
-	[40.672886,-73.969741],
-	[40.671161,-73.961372],
-	[40.669436,-73.962059],
-	[40.664781,-73.961287],
-	[40.663218,-73.961973],
-	[40.663055,-73.963089],
-	[40.654917,-73.962016],
-	[40.650944,-73.972101],
-	[40.658205,-73.974333],
-	[40.660972,-73.979740],
-	[40.673114,-73.969955]
-	],{
-	color: 'red',
-	stroke: 0,
-	fillCollor: '#f03',
-	fillOpacity: 0.5
-	}) .addTo(map);
-
-polygon.bindPopup("<b>Prospect Park</b><br>20 mins walk")
-
-
-
-var polygon = L.polygon([
-	[40.800485,-73.958132],
-	[40.796944,-73.949249],
-	[40.764448,-73.973024],
-	[40.768153,-73.981779]
+$('.office').on('click',function(){
+	map.panTo(new L.LatLng(40.758189,-73.981983),{animate: true, duration: 1.0});
+	$('#image').css('background-image',"url('img/midtown.gif')");
 	
-	],{
-	color: 'red',
-	stroke: 0,
-	fillCollor: '#f03',
-	fillOpacity: 0.5
-	}) .addTo(map);
+});
 
-polygon.bindPopup("<b>Central Park</b><br>20 mins walk")
+$('.pratt').on('click',function(){
+	map.panTo(new L.LatLng(40.691158,-73.963100),{animate: true, duration: 1.0});
+	$('#image').css('background-image',"url('img/pratt.gif')");
+	
+});
+
+$('.home').on('click',function(){
+	map.panTo(new L.LatLng(40.686887,-73.949153),{animate: true, duration: 1.0});
+	$('#image').css('background-image',"url('img/bed.gif')");
+	
+});
 
 
+
+
+//box hover
+
+$('.location').hover(function(){
+	$(this).toggleClass('hover');
+},function(){
+	$(this).toggleClass('hover');
+});
